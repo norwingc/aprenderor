eda6fc916e05b19d78e03d64d9ebbcb80fc27f90
Norwin Guerreo <norwingcruz@gmail.com>
2015-03-19 11:54:41 -0600

    controll REST

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,24 @@
 class ArticlesController < ApplicationController
-	def index		
+	#GET /articles
+	def index	
+		@articles = Article.all	
+	end
+
+	#GET articles/:id
+	def show
+		@article = Article.find(params[:id])		
+	end
+
+	#GET articles/new
+	def new
+		@article = Article.new	
+	end
+
+	#POST articles/create
+	def create
+		@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		@article.save
+
+		redirect_to @article
 	end
 end
